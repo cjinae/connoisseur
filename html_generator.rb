445dc9d33d6a8549b9c1ae34326fee1d838187ec
Christine Lee <christine.jinae@gmail.com>
2013-03-18 10:47:51 -0400

    Set up html generator file

---
+++ html_generator.rb
@@ -0,0 +1,13 @@
+# goes out to API, consuming it and puts-ing it
+
+class HtmlGenerator
+
+	# don't need to initialized because you aren't passing any arguments when you instantiate the HTMLGenerator
+
+	def show
+	end
+
+	def index
+	end
+
+end
--- router.rb
+++ router.rb
@@ -1,1 +1,21 @@
-router.rb
+# Access parameters passed into the app 
+require_relative 'html_generator'
+
+if ARGV.empty? # there are no arguments provided
+	puts "USAGE: bla blah"
+else
+	puts "Yay you passed in some arguments:)"
+	generator = HTMLGenerator.new			# this is instance method. 
+
+	if ARGV[0] == "index"
+		generator.index
+
+	elsif ARGV[0] == "show"
+		generator.show
+	else
+		puts "Unrecognized action. Please refer to usage"
+end	
+
+# puts ARGV[0].inspect 	# arguments
+# puts ARGV[1].inspect
+# puts ARGV[2].inspect
